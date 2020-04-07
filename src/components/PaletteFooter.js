00029095afe342101c28f51f2753e4a529f9a9cd
Shreyas Shukla <shreyas.shukla26@gmail.com>
2020-04-07 21:34:55 +0530

    GoBack Button SCP

--- src/components/PaletteFooter.js
+++ src/components/PaletteFooter.js
@@ -4,8 +4,16 @@ export default function PaletteFooter(props) {
     const { paletteName, emoji } = props;
     return (
         <footer className="Palette__footer">
-            {paletteName}
-            <span className="emoji">{emoji}</span>
+            <span className="Palette__footer--copyright">
+                {' '}
+                &copy; <a href="https://github.com/logicb0mb">
+                    SHREYAS SHUKLA
+                </a>{' '}
+            </span>
+            <div>
+                {paletteName}
+                <span className="emoji">{emoji}</span>
+            </div>
         </footer>
     );
 }
--- src/components/SingleColorPalette.js
+++ src/components/SingleColorPalette.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import { Link } from 'react-router-dom';
 import NavBar from './NavBar';
 import PaletteFooter from './PaletteFooter';
 
@@ -30,7 +31,7 @@ export default class SingleColorPalette extends Component {
     };
     render() {
         const { format } = this.state;
-        const { paletteName, emoji } = this.props.palette;
+        const { paletteName, emoji, id } = this.props.palette;
         const colorBoxes = this._shades.map((color) => (
             <ColorBox
                 key={color.name}
@@ -40,12 +41,19 @@ export default class SingleColorPalette extends Component {
             />
         ));
         return (
-            <div className="Palette">
+            <div className="SingleColorPalette Palette">
                 <NavBar
                     changeColorFormat={this.changeColorFormat}
                     showingAllColors={false}
                 />
-                <div className="Palette__colors">{colorBoxes}</div>
+                <div className="Palette__colors">
+                    {colorBoxes}
+                    <div className="go-back ColorBox">
+                        <Link to={`/palette/${id}`} className="back-button">
+                            Go Back!
+                        </Link>
+                    </div>
+                </div>
                 <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         );
