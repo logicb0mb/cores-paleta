206e91ebceb93a8dd0d1ff83d85393ff6e7403ae
Shreyas Shukla <shreyas.shukla26@gmail.com>
2020-04-14 13:17:32 +0530

    Add NewPaletteForm and Button Styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Palette from './components/Palette';
 import PaletteList from './components/PaletteList';
 import SingleColorPalette from './components/SingleColorPalette';
-
+import NewPaletteForm from './components/NewPaletteForm';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
@@ -20,6 +20,12 @@ class App extends React.Component {
     render() {
         return (
             <Switch>
+                {/* Order does matter in these routes becasue if i will place the new route below the Palette route then it will try for new as a palette id when teh client will visit the url (/new) */}
+                <Route
+                    exact
+                    path="/palette/new"
+                    render={() => <NewPaletteForm />}
+                />
                 <Route
                     exact
                     path="/"
--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -17,7 +17,35 @@ export default {
         display: 'flex',
         width: '100%',
         justifyContent: 'space-between',
-        color: 'white',
+        alignItems: 'center',
+        color: 'black',
+        '& a': {
+            display: 'block',
+            textAlign: 'center',
+            textDecoration: 'none',
+            fontWeight: '800',
+            fontSize: '1em',
+            textTransform: 'uppercase',
+            color: '#fff',
+            borderRadius: '6px',
+            margin: '10px',
+            padding: '1em 3em',
+            backgroundSize: '300% 300%',
+            color: 'white',
+            boxShadow:
+                '0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08)',
+
+            backgroundImage:
+                'linear-gradient(to right, #895cf2 0%, #ffabf4 50%, #895cf2 100%)',
+            transition: '0.5s',
+            backgroundImage:
+                'repeating-linear-gradient(-45deg, #7BDFF2 10px 30px,#B2F7EF 30px 50px,#EFF7F6 50px 70px,#F7D6E0 70px 90px,#F2B5D4 90px 110px)',
+            color: '#130e3a',
+
+            '&:hover': {
+                backgroundPosition: '20% center',
+            },
+        },
     },
     palettes: {
         boxSizing: 'border-box',
