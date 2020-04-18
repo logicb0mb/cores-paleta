9c5074fe7ce5c8852851d6ce624d642ae00f2fd0
Shreyas Shukla <shreyas.shukla26@gmail.com>
2020-04-18 16:07:38 +0530

    Draggable ColorBox_v1

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -16,6 +16,8 @@ import ShuffleIcon from '@material-ui/icons/Shuffle';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import { ChromePicker } from 'react-color';
 
+import DraggableColorBox from './DraggableColorBox';
+
 const drawerWidth = 400;
 
 const useStyles = makeStyles((theme) => ({
@@ -59,6 +61,7 @@ const useStyles = makeStyles((theme) => ({
     },
     content: {
         flexGrow: 1,
+        height: 'calc(100vh -  64px)',
         padding: theme.spacing(3),
         transition: theme.transitions.create('margin', {
             easing: theme.transitions.easing.sharp,
@@ -170,11 +173,10 @@ export default function NewPaletteForm() {
                 })}
             >
                 <div className={classes.drawerHeader} />
-                <ul>
-                    {colors.map((color) => (
-                        <li style={{ backgroundColor: color }}>{color}</li>
-                    ))}
-                </ul>
+
+                {colors.map((color) => (
+                    <DraggableColorBox color={color}></DraggableColorBox>
+                ))}
             </main>
         </div>
     );
